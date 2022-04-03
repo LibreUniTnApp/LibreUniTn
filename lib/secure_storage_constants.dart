371c7bec912e3e38c25ef29f681959739813eaa8
x <x@x>
2022-04-03 23:41:23 +0200

    Updated Android Secure Storage Options

--- lib/secure_storage_constants.dart
+++ lib/secure_storage_constants.dart
@@ -2,6 +2,9 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart'
     show IOSOptions, AndroidOptions;
 
 const iOSOptions = IOSOptions(accountName: 'libreunitn');
-const androidOptions = AndroidOptions(encryptedSharedPreferences: true);
+const androidOptions = AndroidOptions(
+  encryptedSharedPreferences: true,
+  resetOnError: false
+);
 //const webOptions =  WebOptions(dbName: 'libreunitn', publicKey: 'something')
 const credentialKey = 'credentials';
