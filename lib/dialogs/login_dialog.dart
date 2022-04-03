a02743b76bec9f932be68932e9a459713cb8de14
x <x@x>
2022-04-03 23:37:27 +0200

    Updated Dialogs

--- lib/dialogs/login_dialog.dart
+++ lib/dialogs/login_dialog.dart
@@ -1,47 +1,68 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:libreunitn/providers/client/client_manager.dart';
 import 'package:logging/logging.dart';
-import 'package:libreunitn/API/client.dart';
 import 'package:libreunitn/providers/client/client_provider.dart';
 import 'package:libreunitn/providers/invocation_uri.dart';
 import './circular_progress_dialog.dart';
 import './utils.dart';
 
-//TODO: Depend on Inherited widget and schedule Future only once, maybe check whether login has been successful, show error message otherwise
-class LoginDialog extends StatelessWidget {
+class LoginDialog extends StatefulWidget {
+
   const LoginDialog({Key? key}) : super(key: key);
 
   @override
+  State<LoginDialog> createState() => _LoginDialogState();
+}
+
+class _LoginDialogState extends State<LoginDialog> {
+  late final Logger _logger = Logger('App.LoginDialog');
+
+  Future? _loginFuture;
+
+  @override
   Widget build(BuildContext context) {
+    final clientManager = ClientProvider.of(context);
+
     //This will be called only if client is Client
-    final logger = Logger('App.LoginDialog');
-    logger.finest(() => 'client is ${clientManager.client?.runtimeType.toString() ?? 'null'}');
-    final client = clientManager.client as Client;
-    _scheduleLoginFuture(context, client, logger);
-    return CircularProgressDialog(text: Text('Logging in...', style: Theme.of(context).textTheme.headline6));
+    _logger.finest(() => 'client is ${clientManager.client?.runtimeType.toString() ?? 'null'}');
+    _scheduleLoginFuture(context, clientManager);
+    return CircularProgressDialog(
+        text: Text(
+            'Logging in...',
+            style: Theme.of(context).textTheme.headline6
+        )
+    );
   }
 
-  Future<void> _scheduleLoginFuture(BuildContext context, Client client, Logger logger) =>
-    Future(() async {
-      try {
-        final loginRequest = await client.login();
-        logger.fine('Created LoginRequest with authenticationUri ${loginRequest
-            .authenticationUri}');
-        launch(loginRequest.authenticationUri.toString());
-        final response = await invocationUriStream
-            .take(1)
-            .single;
-        logger.fine('LoginRequest Response is ${response ?? 'null'}');
-        if (response != null) {
-          final authClient =
-          await loginRequest.respondWithCustomUri(Uri.parse(response));
-          clientManager.login(authClient);
-          logger.info('Successfully Logged in');
+  void _scheduleLoginFuture(
+      BuildContext context,
+      ClientManager clientManager
+      ) {
+    if(_loginFuture != null) {
+      _loginFuture = Future(() async {
+        try {
+          final loginRequest = await (clientManager.client)!.login();
+          _logger.fine(
+              'Created LoginRequest with authenticationUri ${loginRequest
+                  .authenticationUri}');
+          launch(loginRequest.authenticationUri.toString());
+          final response = await invocationUriStream
+              .take(1)
+              .single;
+          _logger.fine('LoginRequest Response is ${response ?? 'null'}');
+          if (response != null) {
+            final authClient =
+            await loginRequest.respondWithCustomUri(Uri.parse(response));
+            clientManager.login(authClient);
+            _logger.info('Successfully Logged in');
+          }
+        } catch (error) {
+          _logger.severe('Received error(s) while logging in', error);
+        } finally {
+          Navigator.pop(context);
         }
-      } catch (error) {
-        logger.severe('Received error(s) while logging in', error);
-      } finally {
-        Navigator.pop(context);
-      }
-    });
+      });
+    }
+  }
 }
--- lib/dialogs/logout_dialog.dart
+++ lib/dialogs/logout_dialog.dart
@@ -4,50 +4,70 @@ import 'package:logging/logging.dart';
 import 'package:libreunitn/API/authorized_client.dart';
 import 'package:libreunitn/API/client.dart';
 import 'package:libreunitn/providers/client/client_provider.dart';
+import 'package:libreunitn/providers/client/client_manager.dart';
 import 'package:libreunitn/providers/invocation_uri.dart';
 import './circular_progress_dialog.dart';
 import './utils.dart';
 
-//TODO: Depend on Inherited widget and schedule Future only once, maybe check whether logout has been successful, show error message otherwise
-class LogoutDialog extends StatelessWidget {
+class LogoutDialog extends StatefulWidget {
+
   const LogoutDialog({Key? key}) : super(key: key);
 
   @override
+  State<LogoutDialog> createState() => _LogoutDialogState();
+}
+
+class _LogoutDialogState extends State<LogoutDialog> {
+  late final Logger _logger = Logger('App.LogoutDialog');
+
+  Future? _logoutFuture;
+
+  @override
   Widget build(BuildContext context) {
+    final clientManager = ClientProvider.of(context);
+    _logger.finest(() => 'client is ${clientManager.client?.runtimeType.toString() ?? 'null'}');
+
     //This will be called only when client is AuthorizedClient
-    final logger = Logger('App.LogoutDialog');
-    logger.finest(() => 'client is ${clientManager.client?.runtimeType.toString() ?? 'null'}');
-    final client = clientManager.client as AuthorizedClient;
-    _scheduleLogoutFuture(context, client, logger);
-    return CircularProgressDialog(text: Text('Logging out...', style: Theme.of(context).textTheme.headline6));
+    _scheduleLogoutFuture(context, clientManager);
+    return CircularProgressDialog(
+        text: Text(
+            'Logging out...',
+            style: Theme.of(context).textTheme.headline6
+        )
+    );
   }
 
   void _scheduleLogoutFuture(
       BuildContext context,
-      AuthorizedClient client,
-      Logger logger
+      ClientManager clientManager
     ) {
-    Future(() async {
-      try {
-        final logoutRequest = client.logout();
-        logger.fine('LogoutURL is ${logoutRequest.logoutUrl?.toString() ?? 'null'}');
-        if (logoutRequest.logoutUrl != null) {
-          launch(logoutRequest.logoutUrl.toString());
-          final response = await invocationUriStream.take(1).single;
-          logger.fine('LogoutRequest response is ${response ?? 'null'}');
-          if (response != null) {
-            final client = logoutRequest.respond(Uri.parse(response));
-            clientManager.logout(client);
-            logger.info('Successfully Logged out');
+    if(_logoutFuture != null) {
+      _logoutFuture = Future(() async {
+        try {
+          final logoutRequest = (clientManager.client as AuthorizedClient)
+              .logout();
+          _logger.fine(
+              'LogoutURL is ${logoutRequest.logoutUrl?.toString() ?? 'null'}');
+          if (logoutRequest.logoutUrl != null) {
+            launch(logoutRequest.logoutUrl.toString());
+            final response = await invocationUriStream
+                .take(1)
+                .single;
+            _logger.fine('LogoutRequest response is ${response ?? 'null'}');
+            if (response != null) {
+              final client = logoutRequest.respond(Uri.parse(response));
+              clientManager.logout(client);
+              _logger.info('Successfully Logged out');
+            }
+          } else {
+            clientManager.logout(Client());
           }
-        } else {
-          clientManager.logout(Client());
+        } catch (error) {
+          _logger.severe('Received error while logging out', error);
+        } finally {
+          Navigator.pop(context);
         }
-      } catch (error) {
-        logger.severe('Received error while logging out', error);
-      } finally {
-        Navigator.pop(context);
-      }
-    });
+      });
+    }
   }
 }
