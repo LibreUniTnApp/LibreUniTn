93370f2aaf3020b5d81f644ab33b6ac7a59a5a72
x <x@x>
2022-04-05 14:41:03 +0200

    Using Expando-based Weak Reference to determine whether ClientInherited should trigger a rebuild

--- lib/providers/client/client_provider.dart
+++ lib/providers/client/client_provider.dart
@@ -41,17 +41,47 @@ class _ClientProviderState extends State<ClientProvider> {
 
 class _ClientInherited extends InheritedWidget {
   final ClientManager _manager;
-  late final Client? _clientPtr;
+  //TODO: Dart should stabilize WeakRef, for now Expando is the only thing that exposes a similar API
+  /* When _weakClientRef is null, then _manager.client is null too,
+   * otherwise _weakClientRef[client] returns true.
+   * If _manager.client changes (because we logged in
+   * and the old Client instance got dropped), then _weakClientRef won't
+   * keep it alive and it will be collected by the GC as soon as possible.
+   * When the key is dropped, then the value is deleted too.
+   * The idea is that, if this were a Client?, then it wouldn't be dropped
+   * as soon as we change the type and we would have a moment where the two clients
+   * are both in memory with at least a reference each. With a Weak Reference,
+   * we can drop it before */
+  late final Expando<bool>? _weakClientRef;
 
   _ClientInherited({
     Key? key,
     required Widget child,
     required ClientManager manager,
   }): _manager = manager, super(key: key, child: child) {
-    _clientPtr = _manager.client;
+    if(_manager.client != null){
+      _weakClientRef = Expando();
+      _weakClientRef![_manager.client!] = true;
+    } else {
+      _weakClientRef = null;
+    }
   }
 
   @override
-  bool updateShouldNotify(_ClientInherited oldWidget) =>
-      _clientPtr != oldWidget._clientPtr;
+  bool updateShouldNotify(_ClientInherited oldWidget) {
+    final bool oldWasNull = oldWidget._weakClientRef == null;
+    final bool newIsNull = _manager.client == null;
+    if(!oldWasNull && !newIsNull) {
+      /* If the new client is the same as the old one, then _weakClientRef will return true
+       * Otherwise, we haven't registered it to the old Expando and it will return null */
+      return oldWidget._weakClientRef![_manager.client!] ?? false;
+    } else {
+      /* Theoretically, the only other case that might happen is that
+       * the old is null and the new is not. In that case, the first clause
+       * of the || is true and, since the logical operators are short circuiting,
+       * true will be returned immediately.
+       * The other case is added for safety */
+      return (oldWasNull && !newIsNull) || (!oldWasNull && newIsNull);
+    }
+  }
 }
